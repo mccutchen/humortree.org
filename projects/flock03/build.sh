2e58dfbaf5b5bc06e6d2215620eb654bf238ab57
Will McCutchen <mccutchen@gmail.com>
2007-10-08 02:47:20 -0500

    Improvements to build process

--- projects/flock03/build.sh
+++ projects/flock03/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # clean up old class files
-rm flock03/*.class flock03/threads/*.class flock03/util/*.class
+#rm bin/flock03/*.class bin/flock03/threads/*.class bin/flock03/util/*.class
 
 # I'm confused by this classpath stuff.
 # this will compile and run the applet
-javac -cp . flock03/FlockApplet.java && appletviewer index.html
+javac -d bin flock03/FlockApplet.java && appletviewer index.html
