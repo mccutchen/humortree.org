7338512b435a48640e33d688d6131567485cec89
Will McCutchen <mccutchen@gmail.com>
2007-10-08 02:00:53 -0500

    Added some per-Boid settings

--- projects/flock03/flock03/Settings.java
+++ projects/flock03/flock03/Settings.java
@@ -1,14 +1,21 @@
 package flock03;
 
 public class Settings {
-    public static int ANIMATION_DELAY = 100;
+    public static int ANIMATION_DELAY = 30;
     public static int UPDATE_DELAY = ANIMATION_DELAY;
     
-    public static int INITIAL_BOID_COUNT = 10;
-    public static int BOID_SIZE = 10;
+    public static int INITIAL_BOID_COUNT = 100;
+    public static int BOID_SIZE = 12;
     public static int BOID_RADIUS = BOID_SIZE / 2;
+    public static int BOID_TAIL_SIZE = 3;
     
-    public static float MAX_ACCELERATION = 2;
-    public static float MAX_VELOCITY = 5;
+    public static int BOID_MINIMUM_FRIENDLINESS = 5;
+    public static int BOID_MAXIMUM_FRIENDLINESS = 20;
+    
+    public static int BOID_MINIMUM_VISION = 50;
+    public static int BOID_MAXIMUM_VISION = 200;
+    
+    public static float MAX_ACCELERATION = 1;
+    public static float MAX_VELOCITY = 4;
     public static float WANDER_SIZE = 2;
 }
