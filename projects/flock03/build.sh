1a93df31ec0e408d91c21af392a909c0b12fa81f
Will McCutchen <mccutchen@gmail.com>
2007-10-08 01:56:27 -0500

    Cleaned up build script

--- projects/flock03/build.sh
+++ projects/flock03/build.sh
@@ -5,4 +5,4 @@ rm flock03/*.class flock03/threads/*.class flock03/util/*.class
 
 # I'm confused by this classpath stuff.
 # this will compile and run the applet
-javac -cp . -Xlint:deprecation flock03/FlockApplet.java && appletviewer index.html
+javac -cp . flock03/FlockApplet.java && appletviewer index.html
