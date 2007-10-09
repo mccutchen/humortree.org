018977d9e81dce0afc1153b8e63503b1dd4d45a3
Will McCutchen <mccutchen@gmail.com>
2007-10-08 22:15:54 -0500

    Added lineIntersectsCircle method

--- projects/flock03/flock03/util/MathUtils.java
+++ projects/flock03/flock03/util/MathUtils.java
@@ -13,4 +13,21 @@ public class MathUtils {
     public static int wrap(int n, int at) {
     	return (n >= 0 && n < at) ? n : (n < 0) ? at + n : n - at;
     }
+    
+    public static boolean lineIntersectsCircle(int x1, int y1, int x2, int y2, int cx, int cy, int r) {
+        // Adapted from http://www.vb-helper.com/howto_net_line_circle_intersections.html
+        
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        
+        int A = dx*dx + dy*dy;
+        int B = 2 * (dx*(x1 - cx) + dy*(y1 - cy));
+        int C = (x1 - cx)*(x1 - cx) + (y1 - cy)*(y1 - cy) - r*r;
+        int det = B * B - 4 * A * C;
+        
+        if (A <= 1 || det < 0) {
+            return false;
+        }
+        return true;
+    }
 }
