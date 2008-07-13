55c599610965ce4792e80268dc5262275f0ecf52
Will McCutchen <mccutchen@gmail.com>
2008-07-13 15:27:11 -0500

    Tabs -> spaces

--- original/grid/index.php
+++ original/grid/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-    	<title>HUMOR_TREE://GRID [~430 cell table; rough]</title>
-    	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-    	<script language="javascript">		
-    		var coords = [
+        <title>HUMOR_TREE://GRID [~430 cell table; rough]</title>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+        <script language="javascript">      
+            var coords = [
                 0,5,7,12,14,15,16,17,19,20,21,25,26,27,28,31,32,33,34,35,
                 38,39,40,41,42,43,44,46,47,48,49,50,53,54,55,56,57,59,60,
                 61,62,63,66,67,68,69,72,73,74,75,76,79,81,82,83,84,86,91,
@@ -18,19 +18,19 @@
                 375,380,385,390,395,397,398,399,400,401,403,404,405,406,
                 407,410,411,412,413,416,421,423,425,426,427,428
             ];
-    		function light() {
-    		    if (coords.length > 0) {
-    		        var coord = coords.shift();
-    		        $('td').eq(coord)
-    		            .children('img')
-    		            .css('background-color', '#fff');
-    		        setTimeout(light, 10);
-    		    } else {
-    		        $('#button').unbind();
-    		        document.images[0].src = 'next.gif';
-    		        document.images[0].title = "NEXT://";
-    		    }
-    		}
+            function light() {
+                if (coords.length > 0) {
+                    var coord = coords.shift();
+                    $('td').eq(coord)
+                        .children('img')
+                        .css('background-color', '#fff');
+                    setTimeout(light, 10);
+                } else {
+                    $('#button').unbind();
+                    document.images[0].src = 'next.gif';
+                    document.images[0].title = "NEXT://";
+                }
+            }
             $(function() {
                 $('#button').one('click', function() {
                     light();
@@ -38,30 +38,30 @@
                     return false;
                 });
             });
-    	</script>
-    	<style type="text/css">
-    	    body {
-    	        background-color: #999; }
-    	    a#button {
-    	        position: absolute;
-    	        top: 201px;
-    	        left: 775px;
-    	        display: block; }
-    	    a#button img {
-    	        border: none;
-    	        display: block; }
-    	    table {
-    	        position: absolute;
-    	        top: 200px;
-    	        left: 0px; }
-    	    td {
-    	        padding: 1px; }
-    	    td img {
-    	        height: 5px;
-    	        width: 5px;
+        </script>
+        <style type="text/css">
+            body {
+                background-color: #999; }
+            a#button {
+                position: absolute;
+                top: 201px;
+                left: 775px;
+                display: block; }
+            a#button img {
+                border: none;
+                display: block; }
+            table {
+                position: absolute;
+                top: 200px;
+                left: 0px; }
+            td {
+                padding: 1px; }
+            td img {
+                height: 5px;
+                width: 5px;
                 display: block;
-    	        border: 1px solid black; }
-    	</style>
+                border: 1px solid black; }
+        </style>
     </head>
     <body>
         <a id="button" href="../voyeur/"><img src="disp.gif" alt="DISP://"></a>
@@ -69,10 +69,10 @@
             <?php for ($j = 0; $j < 5; $j++) { ?>
             <tr>
                 <?php for ($i = 0; $i < 86; $i++) { ?>
-        		<td><img src="blank.gif"></td>
-        		<?php } ?>
-        	</tr>
-        	<?php } ?>		
+                <td><img src="blank.gif"></td>
+                <?php } ?>
+            </tr>
+            <?php } ?>      
         </table>
     </body>
 </html>
