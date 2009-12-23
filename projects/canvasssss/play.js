030ad3334de42db9c2a9b75e78fd6c8a148249c3
Will McCutchen <will@keyingredient.com>
2009-12-23 12:26:04 -0600

    Add rudimentary support for saving copies of the canvas to a gallery, from which the user can then save copies

--- projects/canvasssss/play.js
+++ projects/canvasssss/play.js
@@ -1,12 +1,28 @@
-function play() {
-    var canvas = document.getElementById('x');
-    canvas.width = window.innerWidth - 200;
-    canvas.height = window.innerHeight - 200;
-
+function play(canvas) {
     var context = canvas.getContext('2d');
     var mover = new RandomMover({ x: canvas.width, y: canvas.height });
     var pen = new SimplePen(context, mover);
     var timer = setInterval(function() { pen.draw() }, 20);
 }
 
-window.addEventListener('load', play, false);
+function save() {
+    var imgdata = this.toDataURL('image/png');
+    var gallery = document.getElementById('gallery');
+    var img = document.createElement('img');
+    img.src = imgdata;
+    gallery.appendChild(img);
+    return false;
+}
+
+function init() {
+    var canvas = document.getElementById('x');
+    var gallery = document.getElementById('gallery');
+    canvas.width = window.innerWidth - 200;
+    canvas.height = window.innerHeight - 200;
+    gallery.style.width = canvas.width + 'px';
+
+    play(canvas);
+    canvas.addEventListener('click', save, false);
+}
+
+window.addEventListener('load', init, false);
